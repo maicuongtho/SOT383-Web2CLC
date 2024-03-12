7f665d18d862f8b7f94e823cab342a3bdc207f85
Mai Cường Thọ <thomc@ntu.edu.vn>
2024-03-12 10:00:48 +0700

    CSS Bootstrap +JQ

---
+++ CSS_JS_IMAGE_Bootrap/src/main/java/thomc/CSS_JS_IMAGE_Bootrap/CssJsImageBootrapApplication.java
@@ -0,0 +1,13 @@
+package thomc.CSS_JS_IMAGE_Bootrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssJsImageBootrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssJsImageBootrapApplication.class, args);
+	}
+
+}
