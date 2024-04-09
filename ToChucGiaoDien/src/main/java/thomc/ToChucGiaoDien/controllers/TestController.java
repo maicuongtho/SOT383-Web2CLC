97674e3104cd3b053620265df0c6d538a7769022
Mai Cường Thọ <thomc@ntu.edu.vn>
2024-04-10 04:16:37 +0700

    Vi du to chuc giao dien thanh cac modeul tai su dung

---
+++ ToChucGiaoDien/src/main/java/thomc/ToChucGiaoDien/controllers/TestController.java
@@ -0,0 +1,26 @@
+package thomc.ToChucGiaoDien.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
+	}
+	@GetMapping("/about")
+	public String about() {
+		return "about.html";
+	}
+	@GetMapping("/service")
+	public String sevrice() {
+		return "service.html";
+	}
+	
+	
+	@GetMapping("/mau")
+	public String mau() {
+		return "page-mau-HTML.html";
+	}
+}
---
+++ ToChucGiaoDien/src/test/java/thomc/ToChucGiaoDien/ToChucGiaoDienApplicationTests.java
@@ -0,0 +1,13 @@
+package thomc.ToChucGiaoDien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToChucGiaoDienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
